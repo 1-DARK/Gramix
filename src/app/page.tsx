d144edcfe08f8665f72bd9c754493ca82ed2329c
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-10 22:13:13 +0530

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
+        <SignInButton mode="modal" />
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+
       <div>hi</div>
     </div>
   );
