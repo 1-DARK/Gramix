445cb1510642d1cca0fb39aada5bd4429d011956
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-10 22:50:14 +0530

    init sheet component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Navbar />
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
