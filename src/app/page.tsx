48c3011069bf60cee83a022390f1b57d7d416624
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-13 22:44:14 +0530

    init user details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
 export default function Home() {
   return (
     <div className="m-4">
