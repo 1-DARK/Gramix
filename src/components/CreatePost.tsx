736cf392e5a3eb00b547b81d659500f9398aba8c
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-14 22:08:48 +0530

    init func

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+
 function CreatePost() {
+  const { user } = useUser();
+  const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
+  const [showImageUpload, setShowImageUpload] = useState(false);
   return <div>hi</div>;
 }
 
