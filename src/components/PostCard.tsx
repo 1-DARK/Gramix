4d7e404e9d1239390170dbd803e7dd85e2a9f1f8
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-18 22:20:21 +0530

    init PostCard comp.

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,4 +1,18 @@
-function PostCard() {
+"use client";
+import { getPosts } from "@/actions/post.action";
+import { Post } from "@/generated/prisma/client";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+
+function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [hasLiked, setHasLiked] = useState(false);
+  const [likes, setLikes] = useState(post._count.likes);
   return <div>Post</div>;
 }
 
