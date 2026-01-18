751cc34463a5a1ff0223e519daf883942ac2cebe
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-18 22:39:03 +0530

    init handlelike function

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,18 +1,40 @@
 "use client";
 import { getPosts } from "@/actions/post.action";
-import { Post } from "@/generated/prisma/client";
 import { useUser } from "@clerk/nextjs";
 import { useState } from "react";
 
 type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
 
 function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
   const { user } = useUser();
   const [newComment, setNewComment] = useState("");
   const [isCommenting, setIsCommenting] = useState(false);
   const [isLiking, setIsLiking] = useState(false);
-  const [hasLiked, setHasLiked] = useState(false);
-  const [likes, setLikes] = useState(post._count.likes);
+  const [hasLiked, setHasLiked] = useState(
+    post.likes.some((like) => like.userId === dbUserId),
+  ); // if any like was made by the current logged-in user
+  const [optimisticLikes, setOptimisticsLikes] = useState(post._count.likes);
+
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setOptimisticsLikes((prev) => prev + (hasLiked ? -1 : 1));
+      await toggleLike(post.id);
+    } catch (error) {
+      setOptimisticsLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+    } finally {
+      setIsLiking(false);
+    }
+  };
+
+  const handleAddComment = async () => {};
+
+  const handleDeletePost = async () => {};
+
   return <div>Post</div>;
 }
 
