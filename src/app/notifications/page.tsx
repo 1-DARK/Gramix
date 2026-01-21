689997205774049e1557c4c115c08bf288a96226
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-21 22:21:13 +0530

    add notification details from notification page

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,5 +1,22 @@
 "use client";
+
+import { getNotifications } from "@/actions/notification.action";
+import { NotificationsSkeleton } from "@/components/NotificationSkeleton";
+import { useEffect, useState } from "react";
+
+type Notifications = Awaited<ReturnType<typeof getNotifications>>;
+type Notification = Notifications[number];
+
 function NotificationsPage() {
+  const [notifications, Setnotifications] = useState<Notification[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {};
+    fetchNotifications();
+  });
+
+  if (isLoading) return <NotificationsSkeleton />;
   return <div></div>;
 }
 
