064a2dfa05b07099ba23ded9780819f7518c5d4b
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-22 23:55:07 +0530

    init loading file

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,5 +1,7 @@
-function ProfilePage({ ...params }: { params: { username: string } }) {
+async function ProfilePage({ ...params }: { params: { username: string } }) {
   console.log(params);
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <h1>hello</h1>;
 }
 
