cde3b361e6eeda1960c4ea5397323f19a12a3ac5
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-23 22:50:05 +0530

    minor change

--- src/app/profile/[username]/page.tsx
+++
@@ -1,8 +0,0 @@
-async function ProfilePage({ ...params }: { params: { username: string } }) {
-  console.log(params);
-
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return <h1>hello</h1>;
-}
-
-export default ProfilePage;
