94fd1169c7801beae6d613cc36881d63f49bce10
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-25 23:31:17 +0530

    implement a minor func

--- src/app/profile/[username]/ProfilePageClient.tsx
+++ src/app/profile/[username]/ProfilePageClient.tsx
@@ -1,4 +1,17 @@
-function ProfilePageClient() {
+"use client";
+
+import { getProfileByUsername, getUserPosts } from "@/actions/profile.action";
+
+type User = Awaited<ReturnType<typeof getProfileByUsername>>;
+type Posts = Awaited<ReturnType<typeof getUserPosts>>;
+
+interface ProfilePageClientProps {
+  user: NonNullable<User>;
+  posts: Posts;
+  likedPosts: Posts;
+  isFollowing: boolean;
+}
+function ProfilePageClient({}: ProfilePageClientProps) {
   return <div></div>;
 }
 
