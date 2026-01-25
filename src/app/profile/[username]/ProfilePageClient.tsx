16bb446d3eccb8afbd72e525e68cd7f61a8e89d0
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-25 23:22:06 +0530

    init profile component

---
+++ src/app/profile/[username]/ProfilePageClient.tsx
@@ -0,0 +1,5 @@
+function ProfilePageClient() {
+  return <div></div>;
+}
+
+export default ProfilePageClient;
--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from "next";
-import { getProfileByUsername } from "@/actions/profile.action";
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
 export async function generateMetadata({
   params,
 }: {
   params: Promise<{ username: string }>;
 }): Promise<Metadata> {
-  const { username } = await params; // ✅ REQUIRED
+  const { username } = await params;
 
   const user = await getProfileByUsername(username);
 
@@ -22,9 +29,29 @@ export async function generateMetadata({
   };
 }
 
-async function ProfilePage({ ...params }: { params: { username: string } }) {
-  console.log(params);
-  return <h1>hello</h1>;
-}
+async function ProfilePageServer({
+  params,
+}: {
+  params: Promise<{ username: string }>;
+}) {
+  const { username } = await params;
+  const user = await getProfileByUsername(username);
 
-export default ProfilePage;
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+export default ProfilePageServer;
