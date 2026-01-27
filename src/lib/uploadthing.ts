c7e419b4c307876ad2e39f5091e0104bedab51f2
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-27 12:43:25 +0530

    init: upload image func

---
+++ src/lib/uploadthing.ts
@@ -0,0 +1,8 @@
+import type { OurFileRouter } from "@/app/api/tasks/uploadthing/core";
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+} from "@uploadthing/react";
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
